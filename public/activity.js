f9d375a3664abd2dc450b4e6b70ff4b4db538868
NAVESANI00 <nicola.avesani@pwc.com>
2020-10-20 15:11:53 +0200

    connection

--- public/activity.js
+++ public/activity.js
@@ -1,8 +1,14 @@
 const postmonger = require("./postmonger");
 
-define([
-    'postmonger'
-], function(Postmonger) {
-    'use strict';
-    var connection = new Postmonger.Session();
+var connection = new Postmonger.Session();
+
+connection.trigger('ready');
+
+connection.on('initActivity', function(data){
+    document.getElementById('configuration').value = JSON.stringify(data, null, 2);
+});
+
+connection.on('clickedNext', function(){
+    var configuration = document.getElementById('configuration').value;
+    connection.trigger('updateActivity', configuration);
 });
